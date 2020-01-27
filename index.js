e47283ca119e3955c8b3f0b78b6c9ad288924e6c
Derek Williams <jogamer130@gmail.com>
2020-01-27 03:41:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+var x;
+
+
+
+
+
+
+
+
+
+
+function theBeatlesPlay(musicians, instruments){
+
+  
+  for (var i = 0; i < 4;){
+ 
+  x == musicians[i] + " plays " + instruments[i];  
+  i++;
+  }
+   return x;
+}
